d1fae185fd7167181d6772a8bfcfcd846971fe07
asadilloobidov201-jpg <asadilloobidov201@gmail.com>
2025-11-30 19:16:37 +0500

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,37 @@
+import telebot
+import requests
+
+BOT_TOKEN = "8292216685:AAHhGto9O_-oSBBe3bkKIe0Pyn7tzJF>
+bot = telebot.TeleBot(BOT_TOKEN)
+
+def download(url):
+    api = "https://api.savein.io/download?url=" + url
+    r = requests.get(api).json()
+
+    try:
+        video = r["result"]["adaptive"][0]["url"]
+        return video
+    except:
+        return None
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    bot.reply_to(msg, "🎥 Menga Instagram, YouTube yoki>
+
+@bot.message_handler(content_types=['text'])
+def get_video(msg):
+    url = msg.text
+    bot.reply_to(msg, "⏳ Yuklab olinmoqda...")
+
+    video = download(url)
+
+    if video:
+        bot.send_video(msg.chat.id, video)
+    else:
+        bot.reply_to(msg, "❌ Video topilmadi. Linkni t>
+
+bot.polling()
+
+bot.py
+main.py
+app.py
